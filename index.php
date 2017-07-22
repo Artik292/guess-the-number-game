eaa0e6ea32efbcd515b69b7a9e6eb5c1e978aa27
Artur Konevnikov <artik292@gmail.com>
2017-07-22 17:54:11 +0300

    adding basic files

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$app = new \atk4\ui\App('Guess the number');
+$app->initLayout('centered');
+
+$secret = rand(1,128);
+echo $secret;
+
+    $a = [];
+    $m_register = new \atk4\data\Model(new \atk4\data\Persistence_Array($a));
+    $m_register->addField('number');
+
+    $f = $app->add(new \atk4\ui\Form(['segment'=>TRUE]));
+    $f->setModel($m_register);
+
+	  $f->onSubmit(function ($f,$secret) {
+        if ($f->model['number'] == '') {
+            return $f->error('number', "Please, enter the number.");
+          } else {
+                  $s = $f->model['number'];
+                  if (ctype_digit($s)) {
+                  if ($s == $secret) {
+                      return "You win";
+                } elseif ($s < $secret) {
+                      return "Your number is smaller";
+                } elseif ($s > $secret) {
+                      return "Your number is bigger";
+                }
+
+          }
+      			//return "Hello $s ";
+			return new \atk4\ui\jsExpression('document.location = "task.php" ');
+		}
+    });
